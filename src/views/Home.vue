c147b14fe6e27151efa1945486a698072c378868
Kunle Oshiyoye <mgenglder@github.com>
2021-12-07 21:19:53 -0500

    Add jiu jitsu card to the mix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <JiuJitsu msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import JiuJitsu from '@/components/JiuJitsu.vue';
 
 @Options({
   components: {
-    HelloWorld,
+    JiuJitsu,
   },
 })
 export default class Home extends Vue {}
--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,13 +1,10 @@
 import { expect } from 'chai';
 import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import JiuJitsu from '@/components/JiuJitsu.vue';
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg },
-    });
-    expect(wrapper.text()).to.include(msg);
+    const wrapper = shallowMount(JiuJitsu);
+    expect(wrapper.text()).to.include('This is the Jiu Jitsu Card');
   });
 });
